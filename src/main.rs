7b08d84d1b6d32d39494c65234fdfd882e0f54c7
Wasin Thonkaew <haxpor@gmail.com>
2022-04-22 18:48:09 +0700

    JSON-based code: add option --out-dir support to write files of JSON-based code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use ::bscscan::bscscan;
 use ::bscscan::environ::Context;
 use clap::Parser;
+use std::path::PathBuf;
 
 #[derive(Debug, Parser)]
 #[clap(author="Wasin Thonkaew (wasin@wasin.io)")]
@@ -23,6 +24,12 @@ struct CommandlineArgs {
     /// Pretty print output for contract ABI. It can only be used if --abi-only exists.
     #[clap(long="no-abi-pretty-print", multiple_values=false, default_missing_value="true", takes_value=false)]
     pub no_abi_pretty_print: bool,
+
+    /// Output directory path to write content of files to. In case of non-JSON format code
+    /// it will use the filename from its contract name, otherwise it will use
+    /// name of each file as seen from JSON data of such code as filename.
+    #[clap(long="out-dir", required=false)]
+    pub out_dir_path: Option<String>,
 }
 
 /// Clean CR/LF as necessary as per platform running the application.
@@ -56,8 +63,78 @@ fn clean_crlf(text: &str) -> String {
     }
 }
 
+/// Combine two path components together and return str version of it.
+///
+/// # Arguments
+/// * `path_a` - first path component
+/// * `path_b` - second path component
+fn combine_two_path_components(path_a: &str, path_b: &str) -> Result<String, String> {
+    let mut path = PathBuf::from(path_a);
+    path.push(path_b);
+
+    match path.as_path().to_str() {
+        Some(res) => Ok(res.to_owned()),
+        None => {
+            let err_msg = format!("Error converting PathBuf to str from result of concatenation of {} and {}", path_a, path_b);
+            return Err(err_msg);
+        }
+    }
+}
+
+/// Create intermediate directories.
+/// It internally handles whether the path is file, or directory. So supplying
+/// the actual filepath here is fine.
+///
+/// # Arguments
+/// * `path` - path to create intermerdiate directories
+fn create_intermediate_dirs(path: &str) -> Result<(), String> {
+    let mut ppath = PathBuf::from(path);
+    // pop the last component out to get only directory path
+    if ppath.file_name().is_some() {
+        ppath.pop();
+    }
+
+    // get path string
+    let ppath_str = match ppath.as_path().to_str() {
+        Some(res) => res,
+        None => {
+            let err_msg = format!("Error getting path string from PathBuf ('{}')", path);
+            return Err(err_msg);
+        }
+    };
+
+    // create all directories leading up to what we will
+    match std::fs::create_dir_all(ppath_str) {
+        Ok(_) => (),
+        Err(e) => {
+            let err_msg = format!("Error creating intermediate directories; err={}", e);
+            return Err(err_msg);
+        }
+    }
+
+    Ok(())
+}
+
+/// Write content to file.
+///
+/// # Arguments
+/// * `filepath` - filepath to write file to, ensure path includes the filename
+/// * `content` - content of file
+fn write_file(filepath: &str, content: &str) -> Result<(), String> {
+    match std::fs::write(filepath, content) {
+        Ok(_) => (),
+        Err(e) => {
+            let err_msg = format!("Error writing file at '{}'; err={}", filepath, e);
+            return Err(err_msg);
+        }
+    }
+
+    Ok(())
+}
+
 fn main() {
     let cmd_args = CommandlineArgs::parse();
+    let has_out_dir_path = cmd_args.out_dir_path.is_some();
 
     // make sure flags are supplied and used only when it's proper
     if !cmd_args.abi_only && cmd_args.no_abi_pretty_print {
@@ -71,11 +148,35 @@ fn main() {
     if cmd_args.abi_only {
         match contracts.get_abi(&ctx, &cmd_args.address, !cmd_args.no_abi_pretty_print) {
             Ok(abi) => {
-                if !cmd_args.no_clean_crlf {
-                    println!("{}", clean_crlf(&abi));
+                if has_out_dir_path {
+                    let out_dir_str = cmd_args.out_dir_path.unwrap();
+                    let write_filepath = match combine_two_path_components(&out_dir_str, "abi.json") {
+                        Ok(res) => res,
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            std::process::exit(1);
+                        }
+                    };
+
+                    match create_intermediate_dirs(&write_filepath) {
+                        Ok(_) => (),
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            std::process::exit(1);
+                        }
+                    }
+
+                    let content = if !cmd_args.no_clean_crlf { clean_crlf(&abi) } else { abi };
+                    match write_file(&write_filepath, &content) {
+                        Ok(_) => println!("{}", &write_filepath),
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            std::process::exit(1);
+                        }
+                    }
                 }
                 else {
-                    println!("{}", abi);
+                    println!("{}", if !cmd_args.no_clean_crlf { clean_crlf(&abi) } else { abi });
                 }
             },
             Err(e) => {
@@ -94,22 +195,47 @@ fn main() {
                     // 1. output all files altogether as a whole
                     // 2. output into target directory by writing into multiple files
                     for i in 1..contract_codes.len() {
-                        println!("// ---------- {} ----------", contract_codes[i].contract_name);
-                        if !cmd_args.no_clean_crlf {
-                            println!("{}", clean_crlf(&contract_codes[i].source_code));
+                        if has_out_dir_path {
+                            let out_dir_str = cmd_args.out_dir_path.as_ref().unwrap();
+                            let write_filepath = match combine_two_path_components(&out_dir_str, &contract_codes[i].contract_name) {
+                                Ok(res) => res,
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    std::process::exit(1);
+                                }
+                            };
+
+                            match create_intermediate_dirs(&write_filepath) {
+                                Ok(_) => (),
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    std::process::exit(1);
+                                }
+                            }
+
+                            let content = if !cmd_args.no_clean_crlf { clean_crlf(&contract_codes[i].source_code) } else { contract_codes[i].source_code.clone() };
+                            match write_file(&write_filepath, &content) {
+                                Ok(_) => println!("{}", &write_filepath),
+                                Err(e) => {
+                                    eprintln!("{}", e);
+                                    std::process::exit(1);
+                                }
+                            }
                         }
                         else {
-                            println!("{}", contract_codes[i].source_code);
+                            println!("// ---------- {} ----------", contract_codes[i].contract_name);
+
+                            if !cmd_args.no_clean_crlf {
+                                println!("{}", clean_crlf(&contract_codes[i].source_code));
+                            }
+                            else {
+                                println!("{}", &contract_codes[i].source_code);
+                            }
                         }
                     }
                 }
                 else {
-                    if !cmd_args.no_clean_crlf {
-                        println!("{}", clean_crlf(&contract_codes[0].source_code));
-                    }
-                    else {
-                        println!("{}", contract_codes[0].source_code);
-                    }
+                    println!("{}", if !cmd_args.no_clean_crlf { clean_crlf(&contract_codes[0].source_code) } else { contract_codes[0].source_code.clone() });
                 }
             },
             Err(e) => {
